f48ddb0b5e02fbb544f1718d0768bd6aab6f973e
MaartenBrijker <maartenbrijker@gmail.com>
2016-05-18 21:33:21 +0200

    multiple files

--- Wauwdio/ViewController.swift
+++ Wauwdio/ViewController.swift
@@ -13,14 +13,38 @@ class ViewController: UIViewController {
 
 
     var oscillator = AKOscillator()
-    var player: AKAudioPlayer!
-    var reverb: AKReverb!
+    var player: AKAudioPlayer?
+    var reverb: AKReverb?
+    var mixer = AKMixer()
+    var drywetmixer = AKDryWetMixer?()
+    
+    var checkie: Bool?
     
     let file = NSBundle.mainBundle().pathForResource("RFX6", ofType: "WAV")
+    let file2 = NSBundle.mainBundle().pathForResource("akonalien", ofType: "mp3")
+    
+
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        player = AKAudioPlayer(file!)
+        
+//        mixer.connect(player!)
+        
+        reverb = AKReverb(player!)
+        let squareWave = AKSquareWaveOscillator()
+
+        drywetmixer = AKDryWetMixer(reverb!, squareWave, balance: 0.5)
+        
+        checkie = true
+        
+        AudioKit.output = drywetmixer
+        AudioKit.start()
+        
         }
     
 
@@ -31,33 +55,41 @@ class ViewController: UIViewController {
     }
     
     @IBAction func reverb(sender: AnyObject) {
-   
-    
+
+        
+        drywetmixer?.balance = random(0, 1)
+
+        
+//        if ((reverb?.isStarted) == true) {
+////            reverb?.stop()
+//        } else {
+//            reverb?.start()
+//        }
         
     }
     
 
     
     func playSound(filename: String, theType: String) {
-        
-        if AudioKit.output == nil {
-            
-            let player = AKAudioPlayer(file!)
-            let reverb = AKReverb(player)
-            let squareWave = AKSquareWaveOscillator()
-            let dryWetMixer = AKDryWetMixer(reverb, squareWave, balance: 0.5)
-            
-            AudioKit.output = dryWetMixer
-            AudioKit.start()
-            
-            if player.isStarted {
-                player.looping = false
-                print("ierer")
-                player.stop()
-            } else {
-                player.looping = true
-                player.play()
-            }
+                
+        if checkie == true {
+            player?.replaceFile(file2!)
+            checkie = false
+            player?.stop()
+        } else {
+            player?.replaceFile(file!)
+            checkie = true
+            player?.stop()
+
         }
+        
+        player?.start()
+        
+//        if player!.isStarted {
+//            player!.stop()
+//        } else {
+//            player!.start()
+//        }
+        
     }
 }
